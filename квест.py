af08cf77470791d7d30c8b1d3cedfdd12522538c
Margo <44144057+mshihailo@users.noreply.github.com>
2023-06-15 21:01:11 +0300

    Update квест.py
    Увидела недочёты исправила

--- квест.py
+++ квест.py
@@ -25,25 +25,25 @@ def start(update, context):
 
 # Обработка команды /help
 def help(update , context):
-context.bot.send_message(chat_id=update.effective_chat.id, text="Для продолжения расследования отправьте команду /search.")
-Обработка команды /search
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Для продолжения расследования отправьте команду /search.")
+#Обработка команды /search
 def search(update, context):
-global current_object_index
+    global current_object_index
 if current_object_index >= len(crime_scene_objects):
-context.bot.send_message(chat_id=update.effective_chat.id, text="Вы прошли все этапы расследования. Отправьте команду /answer, чтобы получить ответ.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Вы прошли все этапы расследования. Отправьте команду /answer, чтобы получить ответ.")
 else:
-current_object = crime_scene_objects[current_object_index]
-context.bot.send_message(chat_id=update.effective_chat.id, text="Исследовать " + current_object + "?")
-current_object_index += 1
+    current_object = crime_scene_objects[current_object_index]
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Исследовать " + current_object + "?")
+    current_object_index += 1
 
 # Обработка команды /suspect
 def suspect(update, context):
-suspects = "Список подозреваемых:\n\n- Джефферсон: автор таинственного письма\n- НИФ-НИФ: жертва убийства\n- НАФ-НАФ: обладатель отпечатков на топоре и следов дезодоранта на рубашке НУФ-НУФа\n- НУФ-НУФ: обладатель перчаток с отпечатками НАФ-НАФа"
-context.bot.send_message(chat_id=update.effective_chat.id, text=suspects)
+    suspects = "Список подозреваемых:\n\n- Джефферсон: автор таинственного письма\n- НИФ-НИФ: жертва убийства\n- НАФ-НАФ: обладатель отпечатков на топоре и следов дезодоранта на рубашке НУФ-НУФа\n- НУФ-НУФ: обладатель перчаток с отпечатками НАФ-НАФа"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=suspects)
 
 #Обработка текстового сообщения пользователя
 def process_text_message(update, context):
-message = update.message.text.lower()
+    message = update.message.text.lower()
 
 if message == "исследовать все":
     current_object_index = len(crime_scene_objects)
@@ -59,8 +59,7 @@ elif message in crime_scene_objects:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Осмотреть " + message + ":\n1. На теле найдены следы дезодоранта.\n2. В кармане брюк обнаружены перчатки с фрагментами отпечатков НАФ-НАФа на внешней стороне.")
     elif message == "комнату":
         context.bot.send_message(chat_id=update.effective_chat.id, text="Осмотреть " + message + ":\n1. Найден топор с отпечатками НАФ-НАФа.")
-    
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Исследовать все объекты или дать ответ?")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Исследовать все объекты или дать ответ?")
 elif message in ["1.1", "1.2", "2.1.1", "2.2.1", "3.2", "4.2"]:
     context.bot.send_message(chat_id=update.effective_chat.id, text="Исследовать место преступления или дать ответ?")
 elif message == "2.1":
@@ -70,14 +69,14 @@ elif message == "2.2":
 elif message == "3.1":
     context.bot.send_message(chat_id=update.effective_chat.id, text="Исследовать НУФ-НУФ по перчаткам или дать ответ?")
 elif message == "4.1":
-context.bot.send_message(chat_id=update.effective_chat.id, text="Исследовать топор или дать ответ?")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Исследовать топор или дать ответ?")
 else:
-context.bot.send_message(chat_id=update.effective_chat.id, text="Команда не распознана. Отправьте команду /search, чтобы продолжить расследование.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Команда не распознана. Отправьте команду /search, чтобы продолжить расследование.")
 
 #Точка входа в программу
 def main():
 # Инициализация бота
-updater = Updater(token='YOUR_BOT_TOKEN', use_context=True)
+updater = Updater(token='6165210897:AAE2WcOc79dclgnmkou35IW4_x58u0LMt3U', use_context=True)
 
 # Регистрация обработчиков команд и текстовых сообщений пользователя
 start_handler = CommandHandler('start', start)
@@ -95,4 +94,4 @@ updater.dispatcher.add_handler(message_handler)
 updater.start_polling()
 updater.idle()
 if name == 'main':
-main()
+    main()
